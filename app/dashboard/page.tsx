1015176e28dc037dc507528e62f7c5971c75cb37
ufukturcan-ntt <ufuk_turcan@hotmail.com>
2026-06-22 00:35:20 +0300

    Create page.tsx

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,14 @@
+export default function DashboardPage() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>EffortEstimator Dashboard</h1>
+
+      <ul>
+        <li>Yeni Çalışma</li>
+        <li>Çalışmalarım</li>
+        <li>Referans Al</li>
+        <li>Sil</li>
+      </ul>
+    </main>
+  );
+}
