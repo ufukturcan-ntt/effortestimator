2a1638ead48232f36515936bf59449f2b1965640
ufukturcan-ntt <ufuk_turcan@hotmail.com>
2026-06-22 00:35:53 +0300

    Create admin page

---
+++ app/admin/page.tsx
@@ -0,0 +1,11 @@
+export default function AdminPage() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>Kullanıcı Onayları</h1>
+
+      <p>
+        Bekleyen kullanıcılar burada listelenecek.
+      </p>
+    </main>
+  );
+}
