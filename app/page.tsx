e852bdeede0ba98437551fb7efff53206ab4701f
ufukturcan-ntt <ufuk_turcan@hotmail.com>
2026-06-22 00:26:55 +0300

    Create home page

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <main style={{ padding: "40px" }}>
+      <h1>EffortEstimator</h1>
+      <p>SAP Sales Effort Estimator</p>
+    </main>
+  );
+}
